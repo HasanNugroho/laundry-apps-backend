058d8f56033bd71cf35fbe26d2a394806f10dda4
HasanNugroho <burhannur19@gmail.com>
2022-02-16 21:38:26 +0700

    update respon riwayat

--- app/Http/Controllers/API/Service/PesananController.php
+++ app/Http/Controllers/API/Service/PesananController.php
@@ -340,7 +340,7 @@ class PesananController extends Controller
             })
             ->where('pesanans.status', 'SELESAI')
             ->where('outlets.id', $user_outlet)
-            ->select('pelanggans.nama', 'pesanans.nota_transaksi', 'pembayarans.status', 'waktus.paket', 'services.nama_layanan', 'outlets.nama_outlet', 'outlets.status_outlet', 'pesanans.created_at', 'pesanans.updated_at')
+            ->select('pesanans.*', 'pelanggans.nama', 'pelanggans.whatsapp', 'pelanggans.alamat', 'outlets.nama_outlet', 'outlets.status_outlet', 'outlets.sosial_media', 'services.nama_layanan', 'services.harga', 'services.kategori', 'services.jenis', 'services.item', 'pembayarans.status as statusPembayaran', 'pembayarans.metode_pembayaran', 'pembayarans.subtotal', 'pembayarans.diskon', 'pembayarans.utang', 'pembayarans.tagihan', 'pembayarans.bayar', 'waktus.nama as nama_waktu', 'waktus.waktu as durasi', 'waktus.paket as paket_waktu', 'waktus.jenis as jenis_waktu')
             ->get();
         }else{
             $pesanan = DB::table('pesanans')
@@ -351,7 +351,7 @@ class PesananController extends Controller
                 ->rightJoin('pembayarans', 'pesanans.id', '=', 'pembayarans.idpesanan')
                 ->where('pesanans.status', 'SELESAI')
                 ->where('outlets.id', $user_outlet)
-                ->select('pelanggans.nama', 'pesanans.nota_transaksi', 'pembayarans.status', 'waktus.paket', 'services.nama_layanan', 'outlets.nama_outlet', 'outlets.status_outlet', 'pesanans.created_at', 'pesanans.updated_at')
+                ->select('pesanans.*', 'pelanggans.nama', 'pelanggans.whatsapp', 'pelanggans.alamat', 'outlets.nama_outlet', 'outlets.status_outlet', 'outlets.sosial_media', 'services.nama_layanan', 'services.harga', 'services.kategori', 'services.jenis', 'services.item', 'pembayarans.status as statusPembayaran', 'pembayarans.metode_pembayaran', 'pembayarans.subtotal', 'pembayarans.diskon', 'pembayarans.utang', 'pembayarans.tagihan', 'pembayarans.bayar', 'waktus.nama as nama_waktu', 'waktus.waktu as durasi', 'waktus.paket as paket_waktu', 'waktus.jenis as jenis_waktu')
                 ->get();
         }
         if($pesanan){
